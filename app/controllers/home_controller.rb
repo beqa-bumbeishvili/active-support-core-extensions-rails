9813c93329cc60c56748033ccc8ec3c858040f1e
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-06-14 19:10:27 +0400

    add view for extensions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
+
   def index
 
   end
+
+  def extensions
+    @conditions_array = []
+
+    params.except(:action, :controller).each do |parameter|
+      @conditions_array << {value: params[parameter], function: parameter, condition: params[parameter].send(parameter) }
+    end
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get 'home/index'
+  post 'home/extensions'
+  root 'home#index'
 end
