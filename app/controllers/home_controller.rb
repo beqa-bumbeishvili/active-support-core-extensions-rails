ebfbcd32dbb541097d70ba52a306bc7fae16c399
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-06-15 08:15:01 +0400

    perform custom calculations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,35 +22,33 @@ class HomeController < ApplicationController
     @calculations[:in_range] = params[:number].to_i.in?(20..50)
     @calculations[:instance_values] = Employee.new.instance_values.to_s.truncate_words(5)
     @calculations[:instance_variable_names] = Employee.new.instance_variable_names
-     # name = SubUser.name
-    # user = User.new('user')
-    # user.name = 'user1'
-    # sub_classes = User.subclasses
-    # descendants = SubUser.descendants
-    # remove =  'Ruby On Rails'.remove(/Ruby ON /)
-    # squish =  '   some    indented string  '.squish
-    # truncate =  'dsasds ds sa s sd saassadds ds sad sa sda'.truncate(10, separator: ' ')
-    # truncate_words =  'quick brows fox jumps over lazy dog'.truncate_words(4)
-    # inquiry = 'random_string'.inquiry.random_string?
-    # starts_with = 'car'.starts_with?('c')
-    # position_at = 'random_string'.at(4)
-    # position_from = 'random_string'.from(6)
-    # position_to = 'random_string'.to(5)
-    # first_letters = 'some_string'.first(4)
-    # last_letters = 'some_string'.last(7)
-    # pluralize = 'apple'.pluralize
-    # singularize = 'people'.singularize
-    # camelize = 'admin_panel'.camelize
-    # underscore ='RubyOnRails'.underscore
-    # titleize = 'game_of_thrones'.titleize
-    # dasherize = 'name_and_lastname'.dasherize
-    # parameterize = 'Will Smith'.parameterize
-    # tableize = 'NcspElement'.tableize
-    # classify = 'contact_info'.classify
-    # constantize = 'Fixnum'.constantize
-    # humanize = 'user_id'.humanize
-    # foreign_key = 'Employee'.foreign_key
-    'debug'
+    @calculations[:sub_classes] = Employee.subclasses
+    @calculations[:descendants] = Employee.descendants
+    @calculations[:remove] =  params[:remove].remove(/[aeiou]/)
+    @calculations[:squish] =  params[:squish].squish
+    @calculations[:truncate] =  params[:truncate].truncate(10, separator: ' ')
+    @calculations[:truncate_words] =  params[:truncate_words].truncate_words(4)
+    @calculations[:inquiry] =  params[:inquiry].inquiry.send(params[:inquiry]+'?')
+    @calculations[:starts_with] =  params[:starts_with].starts_with?('a')
+    @calculations[:position_at] =  params[:position_at].at(4)
+    @calculations[:position_from] =  params[:position_from].from(6)
+    @calculations[:position_to] =  params[:position_to].to(6)
+    @calculations[:first_letters] =  params[:first_letters].first(5)
+    @calculations[:last_letters] =  params[:last_letters].last(5)
+    @calculations[:pluralize] =  params[:pluralize].pluralize
+    @calculations[:singularize] =  params[:singularize].singularize
+    @calculations[:camelize] =  params[:camelize].camelize
+    @calculations[:underscore] =  params[:underscore].underscore
+    @calculations[:titleize] =  params[:titleize].titleize
+    @calculations[:dasherize] =  params[:dasherize].dasherize
+    @calculations[:parameterize] =  params[:parameterize].parameterize
+    @calculations[:tableize] =  params[:tableize].tableize
+    @calculations[:classify] =  params[:classify].classify
+    @calculations[:constantize] =  params[:constantize].constantize
+    @calculations[:humanize] =  params[:humanize].humanize
+    @calculations[:foreign_key] =  params[:foreign_key].foreign_key
+
+
   end
 
 end
